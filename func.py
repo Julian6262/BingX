75507b0f97b2173a97bdcb3ce1f2c80ff2699f45
Julian6262 <d.baburin62@gmail.com>
2025-03-28 01:21:29 +0300

    2

--- func.py
+++ func.py
@@ -1,6 +1,32 @@
 import time
 import hmac
 from hashlib import sha256
+from json import loads
+
+from aiohttp import ClientConnectorDNSError
+
+
+async def send_request(method, session, endpoint, base_url, secret_key, params, quantity=None):
+    url = await make_url(base_url, endpoint, secret_key, params)
+
+    try:
+        async with (session.get(url) if method == "GET" else session.post(url)) as response:
+            if response.status == 200:
+                data = await response.text()
+                json_data = loads(data)
+                return json_data
+            else:
+                print(f"Ошибка : {response.status} для {params['symbol']}")
+                return None
+    except ClientConnectorDNSError:
+        print('Ошибка соединения с сетью')
+
+
+async def make_url(base_url, endpoint, secret_key, params):
+    params_str = await parse_param(params)
+    signature = hmac.new(secret_key.encode("utf-8"), params_str.encode("utf-8"), digestmod=sha256).hexdigest()
+    url = f"{base_url}{endpoint}?{params_str}&signature={signature}"
+    return url
 
 
 async def parse_param(params):
@@ -9,11 +35,3 @@ async def parse_param(params):
         return params_str + "&timestamp=" + str(int(time.time() * 1000))
     else:
         return params_str + "timestamp=" + str(int(time.time() * 1000))
-
-
-async def make_url(base_url, endpoint, secret_key, params):
-    params_str = await parse_param(params)
-    signature = hmac.new(secret_key.encode("utf-8"), params_str.encode("utf-8"), digestmod=sha256).hexdigest()
-    url = f"{base_url}{endpoint}?{params_str}&signature={signature}"
-
-    return url
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 from aiohttp import ClientSession
+from websockets import ConnectionClosed
 from websockets.asyncio.client import connect
 
 from gzip import GzipFile
@@ -8,62 +9,86 @@ from json import dumps, loads
 from os import getenv
 from dotenv import load_dotenv, find_dotenv
 
-from func import make_url
+from func import send_request
 
 load_dotenv(find_dotenv())
 
 BASE_URL = getenv('BASE_URL')
 URL_WS = getenv('URL_WS')
 SECRET_KEY = getenv('SECRET_KEY')
-API_KEY = getenv('API_KEY')
-
-headers = {'X-BX-APIKEY': API_KEY}
+HEADERS = {'X-BX-APIKEY': getenv('API_KEY')}
 
 
-async def get_price(session, symbol):  # получить цену спота через http
+async def price_updates(symbol, interval_seconds):
+    method = "GET"
     endpoint = '/openApi/spot/v1/ticker/price'
     params = {
         "symbol": symbol
     }
-    url = await make_url(BASE_URL, endpoint, SECRET_KEY, params)
 
-    async with session.get(url) as response:
-        if response.status == 200:
-            data = await response.json()
-            return float(data["data"][0]["trades"][0]["price"])
-        else:
-            print(f"Error: {response.status} for {symbol}")
-            return None
-
-
-async def price_updates(symbol, interval_seconds):
-    async with ClientSession(headers=headers) as session:
+    async with ClientSession(headers=HEADERS) as session:
         while True:
-            price = await get_price(session, symbol)
-            if price:
+            data = await send_request(method, session, endpoint, BASE_URL, SECRET_KEY, params)
+            if data:
+                price = float(data["data"][0]["trades"][0]["price"])
                 print(f"{symbol}: {price}")
             await asyncio.sleep(interval_seconds)
 
 
-async def ticket(symbol):
+async def get_symbol_info(symbol):
+    method = "GET"
+    endpoint = '/openApi/spot/v1/common/symbols'
+    params = {
+        "symbol": symbol
+    }
+
+    async with ClientSession(headers=HEADERS) as session:
+        price = await send_request(method, session, endpoint, BASE_URL, SECRET_KEY, params)
+        if price:
+            print(f"{symbol}: {price}")
+
+
+async def place_order(symbol, quantity):    #  проверить без сети
+    method = "POST"
+    endpoint = '/openApi/spot/v1/trade/order'
+    params = {
+        "symbol": symbol,
+        "type": "MARKET",
+        "side": "BUY",
+        "quantity": quantity,
+    }
+
+    async with ClientSession(headers=HEADERS) as session:
+        price = await send_request(method, session, endpoint, BASE_URL, SECRET_KEY, quantity, params)
+        if price:
+            print(f"{symbol}: {price}")
+
+
+async def price_updates_ws(symbol):
     channel = {"id": "1", "reqType": "sub", "dataType": f"{symbol}@lastPrice"}
 
-    async with connect(URL_WS) as websocket:
-        await websocket.send(dumps(channel))
-        async for message in websocket:
-            compressed_data = GzipFile(fileobj=BytesIO(message), mode='rb')
-            decompressed_data = compressed_data.read()
-            utf8_data = loads(decompressed_data.decode('utf-8'))
-            if 'data' in utf8_data:
-                price = float(utf8_data["data"]["c"])
-                print(price)
+    async for websocket in connect(URL_WS):
+        try:
+            await websocket.send(dumps(channel))
+            async for message in websocket:
+                compressed_data = GzipFile(fileobj=BytesIO(message), mode='rb')
+                decompressed_data = compressed_data.read()
+                utf8_data = loads(decompressed_data.decode('utf-8'))
+                if 'data' in utf8_data:
+                    price = float(utf8_data["data"]["c"])
+                    print(price)
+        except ConnectionClosed:
+            print('Ошибка соединения с сетью')
+            continue
 
 
 async def main():
     tasks = [
         # price_updates("BTC_USDT", 15),
         # price_updates("ETH_USDT", 10),
-        ticket("BTC-USDT")
+        # get_symbol_info("SOL-USDT"),
+        price_updates_ws("BTC-USDT")
+        # place_order('BTC-USDT', 0.000011)
     ]
 
     await asyncio.gather(*tasks)
